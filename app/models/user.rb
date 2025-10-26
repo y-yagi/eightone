0d976a2a8476b0ff5bcfc015cd10d5a4c068e12f
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2025-10-26 18:42:29 +0900

    Add example for "Deprecated Associations"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, deprecated: true
+
   def to_markdown
     email
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_26_062738) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_26_093714) do
+  create_table "posts", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
